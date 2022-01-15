f796d71014528c590c7ba0c2aab77c02852374b6
AlexKjava <a79060898849@gmail.com>
2022-01-15 23:34:33 +0300

    first

---
+++ MedAppA/src/main/java/MedAppMain.java
@@ -0,0 +1,7 @@
+package MedAppA.src.main.java;
+
+public class MedAppMain {
+	public static void main(String[] args) {
+		System.out.println("TST");
+	}
+}
---
+++ MedAppA/src/test/java/Test.java
@@ -0,0 +1,9 @@
+package MedAppA.src.test.java;
+
+public class Test {
+	//@Test
+	public void test() {
+		// This line will be written on the server console.
+		System.out.println("Test is invoked...");
+	}
+}
